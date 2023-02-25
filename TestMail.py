960478b048da0c9d0985a60a89b2d71dfbadd8e4
clear-312 <122206931+clear-312@users.noreply.github.com>
2023-02-25 17:30:14 +0545

    Update TestMail.py

--- TestMail.py
+++ TestMail.py
@@ -38,4 +38,4 @@ def process():
     return send_from_directory(app.config['DOWNLOAD_FOLDER'].resolve().as_posix(), 'line_art.jpg', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
